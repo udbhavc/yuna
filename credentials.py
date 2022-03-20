24f4f5e26a664e27f586b27af292ca8340d72223
udbhavc <2019umt1269@mnit.ac.in>
2022-03-21 03:50:43 +0530

    first commit

---
+++ credentials.py
@@ -0,0 +1,4 @@
+API_key = "gYv91zIuXVSq6I4UWSNCWhdtm"
+API_secret_key= "ZLWALVA4mpNGO4MARfTcnh7Jc8MYtitIFXjCU4hYMA0698ViTD"
+access_token ="1497207507864809477-cS7e2drlXjznkFZbclL7ph19fZX4HH" 
+access_token_secret ="PDHEKsRoRllJ4nJIIqUHlSZNX9QfSykbbMUrS47FVpYrN"
